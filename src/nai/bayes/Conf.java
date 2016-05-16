8feb0b2574000431edb0beef77e7d03780c2f1ed
MarsNobu <marsprivate+web@outlook.com>
2016-05-16 21:35:48 +0200

    Add Config class

---
+++ src/nai/bayes/Conf.java
@@ -0,0 +1,7 @@
+package nai.bayes;
+
+public class Conf {
+	public static final String TRAIN_FILE = "data/train.data";
+	public static final String TEST_FILE = "data/test.data";
+	public static final String DATA_FILE = "data/spambase.data";
+}
