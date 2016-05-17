591535e31eac37d1f477b1af110f7ad19b8bc006
MarsNobu <marsprivate+web@outlook.com>
2016-05-17 17:51:40 +0200

    Make changes to Data class

--- src/nai/bayes/BayesClassifier.java
+++ src/nai/bayes/BayesClassifier.java
@@ -4,4 +4,12 @@ public class BayesClassifier {
 	
 	private DataSet ds = new DataSet();
 	
+	public void train() {
+		Data data = ds.getTrainSet();
+	}
+	
+	public void test() {
+		
+	}
+	
 }
--- src/nai/bayes/Data.java
+++ src/nai/bayes/Data.java
@@ -4,10 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Data {
-	public final List<Double[]> x = new ArrayList<>();
-	public final List<Integer> d = new ArrayList<>();
+	private final List<Double[]> x = new ArrayList<>();
+	private final List<Integer> d = new ArrayList<>();
 	
 	public int size() {
 		return x.size();
+	}
+	
+	public Double[] xAt(int index) {
+		return x.get(index);
+	}
+	
+	public int dAt(int index) {
+		return d.get(index);
+	}
+	
+	public void addX(Double[] x) {
+		this.x.add(x);
+	}
+	
+	public void addD(Integer d) {
+		this.d.add(d);
 	}
 }
