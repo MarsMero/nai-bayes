d9c05b698fa7ee56a39bbd33c41c21264b6fd2c4
marcinsekowski <marcinsekowski@gmail.com>
2016-06-08 22:54:19 +0200

    added load and filter methods

--- src/nai/bayes/DataSet.java
+++ src/nai/bayes/DataSet.java
@@ -1,32 +1,53 @@
 package nai.bayes;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
-public class DataSet<T> implements IDataSet<T> {
+public class DataSet implements IDataSet<String> {
+
+	private int attrs;
+	private final List<Data<String>> list = new ArrayList<>();
 	
-	public DataSet() {
-		
+	public DataSet() throws IOException {
+		this(Conf.DATA_FILE);
 	}
 	
-	public DataSet(String dataSetFile) {
-		
+	public DataSet(String dataSetFile) throws IOException {
+		BufferedReader dataSetReader = new BufferedReader(new FileReader(dataSetFile));
+		String line;
+		String[] array = null;
+		while((line = dataSetReader.readLine()) != null) {
+			array = line.split(",");
+			if(array.length > 1) {
+				Data<String> data = new Data<>();
+				data.setLabel(array[array.length-1]);
+				for (int i = 0; i < array.length-1; i++) {
+					data.addAttr(array[i]);
+				}
+			}
+		}
+		dataSetReader.close();
+		if(array == null || array.length == 0)
+			attrs = 0;
+		else
+			attrs = array.length - 1;
 	}
 	
 	@Override
-	public List<Data<T>> getSet() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Data<String>> getSet() {
+		return Collections.unmodifiableList(list);
 	}
 
 	@Override
 	public int nAttrs() {
-		// TODO Liczba atrybutow bez okladki
-		return 0;
+		return attrs;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return list.size();
 	}
 }
--- src/nai/bayes/DataSetUtils.java
+++ src/nai/bayes/DataSetUtils.java
@@ -1,7 +1,66 @@
 package nai.bayes;
 
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+import weka.core.converters.CSVSaver;
+import weka.core.converters.ConverterUtils.DataSource;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Discretize;
+import weka.filters.unsupervised.attribute.RemoveUseless;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.*;
+
 public class DataSetUtils {
 	// filters from weka.filters.unsupervised.attribute
 	// for example RemoveUseless
 	// and Discretize
+
+    public static void split(String dataSetFile, String trainSetFile, String testSetFile) throws IOException {
+        BufferedReader dataSetReader = new BufferedReader(new FileReader(dataSetFile));
+        PrintWriter trainSetWriter = new PrintWriter(new File(trainSetFile));
+        PrintWriter testSetWriter = new PrintWriter(new File(testSetFile));
+        List<String> lines = new LinkedList<>();
+        String line;
+        while ((line = dataSetReader.readLine()) != null) {
+            lines.add(line);
+        }
+        dataSetReader.close();
+        int testSize = (int)(lines.size()*0.8);
+        Random random = new Random();
+        for (int i = 0; i < testSize; i++) {
+            trainSetWriter.println(lines.remove(random.nextInt(lines.size())));
+        }
+        while (!lines.isEmpty()) {
+            testSetWriter.println(lines.remove(0));
+        }
+        trainSetWriter.flush();
+        testSetWriter.flush();
+        trainSetWriter.close();
+        testSetWriter.close();
+    }
+
+    public static void filter(String dataSetFile, String outputSetFile) throws Exception {
+        CSVLoader csvLoader = new CSVLoader();
+        csvLoader.setNoHeaderRowPresent(true);
+        csvLoader.setFile(new File(dataSetFile));
+        DataSource source = new DataSource(csvLoader);
+        Instances data = source.getDataSet();
+        data.setClassIndex(data.numAttributes() - 1);
+        RemoveUseless removeUseless = new RemoveUseless();
+        removeUseless.setInputFormat(data);
+        data = Filter.useFilter(data, removeUseless);
+        /*Discretize discretize = new Discretize();
+        discretize.setInputFormat(data);
+        data = Filter.useFilter(data, discretize);*/
+        File file = new File(outputSetFile);
+        Files.deleteIfExists(file.toPath());
+        CSVSaver csvSaver = new CSVSaver();
+        csvSaver.setFile(file);
+        csvSaver.setNoHeaderRow(true);
+        csvSaver.setInstances(data);
+        csvSaver.writeBatch();
+    }
+
 }
